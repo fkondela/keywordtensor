c61774f53983732280dbd6b5ae088238f2a89998
Fabian Kondela <fabiankondela@gmail.com>
2026-07-13 22:31:01 +0200

    fix: ensure python path resolution in streamlit cloud

--- demo/app.py
+++ demo/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, RTCConfiguration, WebRtcMode
 from keywordtensor.core import Engine
